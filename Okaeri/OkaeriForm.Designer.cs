0270ead7b60591d44c09e2aa931b56f21925fad0
G2 <g2simm+FromSourceTree@gmail.com>
2014-05-02 02:13:40 +0900

    first commit

---
+++ Okaeri/OkaeriForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Okaeri
+{
+    partial class OkaeriForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OkaeriForm));
+            this.notifyIconOkaeri = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripOkaeri = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboVoice = new System.Windows.Forms.ComboBox();
+            this.fileOpenButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStripOkaeri.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIconOkaeri
+            // 
+            this.notifyIconOkaeri.ContextMenuStrip = this.contextMenuStripOkaeri;
+            this.notifyIconOkaeri.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconOkaeri.Icon")));
+            this.notifyIconOkaeri.Text = "notifyIconOkaeri";
+            this.notifyIconOkaeri.Visible = true;
+            this.notifyIconOkaeri.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconOkaeri_MouseDoubleClick);
+            // 
+            // contextMenuStripOkaeri
+            // 
+            this.contextMenuStripOkaeri.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStripOkaeri.Name = "contextMenuStripOkaeri";
+            this.contextMenuStripOkaeri.Size = new System.Drawing.Size(108, 48);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.menuToolStripMenuItem.Text = "menu";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.startButton.Location = new System.Drawing.Point(28, 113);
+            this.startButton.Margin = new System.Windows.Forms.Padding(20);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(260, 70);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.comboVoice);
+            this.groupBox1.Controls.Add(this.fileOpenButton);
+            this.groupBox1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 84);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setting";
+            // 
+            // comboVoice
+            // 
+            this.comboVoice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboVoice.DisplayMember = "おかえり! [Okaeri!]";
+            this.comboVoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboVoice.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboVoice.FormattingEnabled = true;
+            this.comboVoice.Location = new System.Drawing.Point(16, 18);
+            this.comboVoice.Name = "comboVoice";
+            this.comboVoice.Size = new System.Drawing.Size(288, 26);
+            this.comboVoice.TabIndex = 6;
+            this.comboVoice.ValueMember = "0";
+            // 
+            // fileOpenButton
+            // 
+            this.fileOpenButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.fileOpenButton.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.fileOpenButton.Location = new System.Drawing.Point(16, 50);
+            this.fileOpenButton.Name = "fileOpenButton";
+            this.fileOpenButton.Size = new System.Drawing.Size(143, 28);
+            this.fileOpenButton.TabIndex = 3;
+            this.fileOpenButton.Text = "Select Other Voice";
+            this.fileOpenButton.UseVisualStyleBackColor = true;
+            this.fileOpenButton.Click += new System.EventHandler(this.fileOpenButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Okaeri.Properties.Resources._001mojib;
+            this.pictureBox1.Location = new System.Drawing.Point(337, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 161);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OkaeriForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 201);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 240);
+            this.Name = "OkaeriForm";
+            this.Text = "Okaeri!";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OkaeriForm_FormClosed);
+            this.Load += new System.EventHandler(this.OkaeriForm_Load);
+            this.contextMenuStripOkaeri.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconOkaeri;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripOkaeri;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button fileOpenButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboVoice;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Okaeri/OkaeriForm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Microsoft.Win32;
+
+namespace Okaeri
+{
+    public partial class OkaeriForm : Form
+    {
+        private string userfileName = "";
+
+
+        public OkaeriForm()
+        {
+            InitializeComponent();
+            SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+        }
+
+        private void OkaeriForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notifyIconOkaeri.Visible = false;
+            SystemEvents.PowerModeChanged -= new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Activate();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIconOkaeri.Visible = false;
+            try
+            {
+                Application.Exit();
+            }
+            catch
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        private void notifyIconOkaeri_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Activate();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if((string)comboVoice.SelectedValue == "userfiles"){
+                if (!(System.IO.File.Exists(userfileName))){
+                    MessageBox.Show("The voice file was not found.\n Please check file exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+                }
+            }
+            
+            this.Visible = false;
+            
+        }
+
+        private void OkaeriForm_Load(object sender, EventArgs e)
+        {
+
+            var voiceTable = new DataTable();
+            voiceTable.Columns.Add("ID", typeof(string));
+            voiceTable.Columns.Add("NAME", typeof(string));
+
+            var dict = new Dictionary<string, string>(){
+                {"okaeri_03.wav","おかえり! [Okaeri!]"},
+                {"okaerinasai_02.wav","おかえりなさい [Okaerinasai]"},
+                {"ohayo_01.wav","おはよっ [Ohayo]"},
+                {"ohayougozaimasu_02.wav","おはようございます [Ohayou gozaimasu]"},
+                {"ohayougozaimasu_03.wav","おはようございまーす [Ohayou gozaimaaasu]"},
+                {"ohhaa_01.wav","おっはー! [Ohhaa!]"},
+                {"ohhaa_02.wav","おっ...はあぁぁ... [O...haaa...]"},
+                {"ohhayoo_01.wav","おっはよー! [Ohhayoo!]"},
+                {"ohhayoo_02.wav","お...はよぉぉ... [O...hayoo...]"},
+                {"ohisashiburi_03.wav","おひさしぶり [Ohisashiburi]"},
+                {"ohisashiburidesu_03.wav","おひさしぶりですっ [Ohisashiburidesu]"},
+                {"userfiles","選択したファイル [User Select]"}
+            };
+
+            foreach (var data in dict)
+            {
+                var row = voiceTable.NewRow();
+                row["ID"] = data.Key;
+                row["NAME"] = data.Value;
+                voiceTable.Rows.Add(row);
+            }
+            voiceTable.AcceptChanges();
+            comboVoice.DataSource = voiceTable;
+            comboVoice.DisplayMember = "NAME";
+            comboVoice.ValueMember = "ID";
+        }
+
+        private void fileOpenButton_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.Filter = "wavファイル|*.wav";
+
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                userfileName = this.openFileDialog1.FileName;
+                comboVoice.SelectedValue = "userfiles";
+            }
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            if (e.Mode == PowerModes.Resume)
+            {
+                string directryName = @"voices/";
+                try
+                {
+                    if ((string)comboVoice.SelectedValue == "userfiles")
+                        PlayMusic(userfileName);
+                    else
+                        PlayMusic(directryName + (string)comboVoice.SelectedValue);
+                }
+                catch { }
+            }
+        }
+
+
+        private void PlayMusic(string fileName)
+        {
+            if (System.IO.File.Exists(fileName))
+            {
+                new SoundPlayer(fileName).Play();
+            }
+        }
+    }
+}
